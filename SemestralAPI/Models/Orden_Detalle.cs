561e5ba5ec267be919915ffdad3af047e24ac744
Karkinox13 <joniel2309@gmail.com>
2025-12-10 17:48:12 -0500

    Error Rama
    me equivoque de rama ya lo corrijo En orden y transacciones

--- SemestralAPI/Models/Orden_Detalle.cs
+++ SemestralAPI/Models/Orden_Detalle.cs
@@ -1,12 +1,10 @@
 ﻿namespace SemestralAPI.Models {
   public class Orden_Detalle {
-    public int Id { get; set; }
-    public int Orden_Id { get; set; }
-    public int Articulo_Id { get; set; }
-    public int Cantidad { get; set; }
-    public DateTime Created_At { get; set; }
-    public DateTime Updated_At { get; set; }
-    public decimal Precio_Unitario { get; set; }
-    public decimal Precio_Final { get; set; }
+    public required int Id { get; set; }
+    public required int OrdenId { get; set; }
+    public required int ArticuloId { get; set; }
+    public required int Cantidad { get; set; }
+    public required decimal PrecioUnitario { get; set; }
+    public required decimal PrecioFinal { get; set; }
   }
 }
